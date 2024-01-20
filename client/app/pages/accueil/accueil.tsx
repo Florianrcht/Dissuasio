558a9d8a0fe4b84157ec714502fbe5695471eae3
Floria <florian.richet2004@gmail.com>
2024-01-20 19:59:04 +0100

    v1 - Refonte architecture

---
+++ client/app/pages/accueil/accueil.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Accueil: React.FC = () => {
+  return (
+    <main>
+      <p>
+        Accueil
+      </p>
+    </main>
+  );
+};
+
+export default Accueil;
---
+++ client/app/pages/composants/header.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header: React.FC = () => {
+    return (
+        <header>
+            <title>Dissuasio</title>
+            <h1>Mon Header</h1>
+        </header>
+    );
+};
+
+export default Header;
