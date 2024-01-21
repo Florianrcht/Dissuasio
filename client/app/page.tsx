305be7807562d949eb22456b8ab43baee2e9b331
Floria <florian.richet2004@gmail.com>
2024-01-21 14:25:34 +0100

    supp css index

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,8 +1,8 @@
-import styles from "./page.module.css";
+import "./page.css";
 import React from "react";
 export default function Home() {
   return (
-    <main className={styles.main}>
+    <main className="main_accueil">
       <p>Index</p>
     </main>
   );
