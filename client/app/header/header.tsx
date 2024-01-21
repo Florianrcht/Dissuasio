59b2bf40d8e6f5c8a4508a37bfdafdd3893a5e89
Floria <florian.richet2004@gmail.com>
2024-01-21 13:04:21 +0100

    V1 - Header

--- client/app/header/header.tsx
+++ client/app/header/header.tsx
@@ -1,9 +1,18 @@
+import './header.css';
 import React from 'react';
 
 const Header: React.FC = () => {
     return (
         <header>
-            <h1>Mon Header</h1>
+            <div className='header_div'>
+                <a href="#">ACCUEIL</a>
+                <a href="#">NOS BASES</a>
+                <h1>Dissuasio</h1>
+                <a href="#">ACTU</a>
+                <a href="#">OPEX</a>
+            </div>
+            <div className='header_border'>
+            </div>
         </header>
     );
 };
