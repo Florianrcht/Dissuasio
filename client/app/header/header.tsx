ad1862b4ae121c5bc2383a1c6a8b83f98f1af8e9
Floria <florian.richet2004@gmail.com>
2024-01-21 11:35:10 +0100

    Patch pour App Routeur

--- client/app/header/header.tsx
+++ client/app/header/header.tsx
@@ -3,7 +3,6 @@ import React from 'react';
 const Header: React.FC = () => {
     return (
         <header>
-            <title>Dissuasio</title>
             <h1>Mon Header</h1>
         </header>
     );
---
+++ client/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+import React from "react";
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <p>Index</p>
+    </main>
+  );
+}
