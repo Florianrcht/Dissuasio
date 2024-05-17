ddbea4fd84fef979a31ed661e9f72b004083f6f2
Floria <florian.richet2004@gmail.com>
2024-05-17 18:57:08 +0200

    ^

--- server/server.js
+++ server/server.js
@@ -25,12 +25,12 @@ const getFormattedDate = () => {
   return `${now.getFullYear()}-${(now.getMonth() + 1).toString().padStart(2, '0')}-${now.getDate().toString().padStart(2, '0')} ${now.getHours().toString().padStart(2, '0')}:${now.getMinutes().toString().padStart(2, '0')}:${now.getSeconds().toString().padStart(2, '0')}`;
 };
 
-httpsServer.listen(port, '82.124.211.87', () => {
-  console.log(`<Serveur Node écoute sur https://82.124.211.87:${port}>`);
+httpsServer.listen(port, '0.0.0.0', () => {
+  console.log(`<Serveur Node écoute sur http://0.0.0.0:${port}>`);
 });
 
 //#region UNITES/CARTE
-app.get('http://82.124.211.87:3001/api/PostTwitter/GetAll', async (req, res) => {
+app.get('https://82.124.211.87:3001/api/PostTwitter/GetAll', async (req, res) => {
   try {
         const unites = await prisma.unites_armee_terre.findMany();
         console.log(`==> SUCCES CARTE | GET ALL PRISMA unites_armee_terre | ${getFormattedDate()} | ${unites.length} Éléments`);
